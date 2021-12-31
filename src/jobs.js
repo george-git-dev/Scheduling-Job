d30cbaacc666a55f13475995bb70cdd80212a7dd
George <george_roots@hotmail.com>
2021-12-31 00:23:46 -0300

    Add dados Json

--- src/jobs.js
+++ src/jobs.js
@@ -1,2 +1,8 @@
 // Scheduling Job
 
+/*Com os dados Json recebidos e armazenados na variável Job*/
+let Job = [
+
+    { "ID": 1, "Descricao": "Importação de arquivos de fundos",   "DataMaxima": "2019-11-10 12:00:00", "tempo": 2 }, 
+    { "ID": 2, "Descricao": "Importação de dados da Base Legada", "DataMaxima": "2019-11-11 12:00:00", "tempo": 4 }, 
+    { "ID": 3, "Descricao": "Importação de dados de integração",  "DataMaxima": "2019-11-11 08:00:00", "tempo": 6 }]
