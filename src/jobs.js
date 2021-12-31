95afc0efd4a2969deda2dc39894387d38969e8a5
George <george_roots@hotmail.com>
2021-12-31 00:27:04 -0300

    Add objeto Date

--- src/jobs.js
+++ src/jobs.js
@@ -6,3 +6,12 @@ let Job = [
     { "ID": 1, "Descricao": "Importação de arquivos de fundos",   "DataMaxima": "2019-11-10 12:00:00", "tempo": 2 }, 
     { "ID": 2, "Descricao": "Importação de dados da Base Legada", "DataMaxima": "2019-11-11 12:00:00", "tempo": 4 }, 
     { "ID": 3, "Descricao": "Importação de dados de integração",  "DataMaxima": "2019-11-11 08:00:00", "tempo": 6 }]
+
+  
+
+/* Objeto Date criado com os parametros estabelecidos com data de inicio e fim*/
+
+// Data inicio
+const janelaInicio = new Date('2019-11-10 09:00:00');
+// Data final
+const janelaFim = new Date('2019-11-11 12:00:00');
