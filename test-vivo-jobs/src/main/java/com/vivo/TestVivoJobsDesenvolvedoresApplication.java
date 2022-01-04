143f363b1aa06f5ba0f59d1a1fa02f8580226014
George <george_roots@hotmail.com>
2022-01-04 17:17:43 -0300

    initial commit

---
+++ test-vivo-jobs/src/main/java/com/vivo/TestVivoJobsDesenvolvedoresApplication.java
@@ -0,0 +1,13 @@
+package com.vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVivoJobsDesenvolvedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestVivoJobsDesenvolvedoresApplication.class, args);
+	}
+
+}
